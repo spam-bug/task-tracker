91cc0c5ea811f45f02c1cdbf12df36cfef903b91
Raymark Peña <dev.mlimp@gmail.com>
2021-04-19 03:43:36 +0800

    Create Tasks

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 
 </template>
 
 <script>
-import Header from "./components/Header";
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Apointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 3rd at 11:00am',
+        reminder: false,
+      },
+    ]
   }
 
 }
